d16e6bd32d931ea9a2cc65e2c3725cf63b926301
MattiaCodazzi <m.codazzi3@studenti.unibg.it>
2025-08-02 17:45:47 +0200

    grdhtrd

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,3 +4,9 @@ from . import views
 urlpatterns = [
     # path('', views.ArtworkListView.as_view(), name='artwork-list'),
 ]
+
+from .views import DashboardView
+urlpatterns = [
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    # … altre rotte …
+]
--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,5 @@
 # Placeholder per le class-based views CRUD
+from django.views.generic import TemplateView
+
+class DashboardView(TemplateView):
+    template_name = "dashboard.html"
